5f0e5b660443c0013f528205c5d66536033ad865
Mladen Janjetovic <mladen@tirien.com>
2016-09-29 11:55:03 +0200

    generate Nested Page List trait

--- src/Traits/HelpersTrait.php
+++ src/Traits/HelpersTrait.php
@@ -18,4 +18,24 @@ trait HelpersTrait
     {
         return trim(strtolower(preg_replace('/[^a-zA-Z0-9_]/', '', $element)), '_');
     }
+    
+    public function generateNestedPageList($pages, $navigation = '')
+    {
+        $navigation .= '<ul>';
+
+        foreach ($pages as $page) {
+            $navigation .= '<li>';
+            $navigation .= '<a href="pages/edit/'.$page->id.'"><b>'.$page->title.'</a>';
+
+            if ($page->subpages()->count()) {
+                $navigation = $this->generateNestedPageList($page->subpages(), $navigation);
+            }
+
+            $navigation .= '</li>';
+        }
+        
+        $navigation .= '</ul>';
+
+        return $navigation;
+    }
 }
