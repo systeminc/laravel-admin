13d26883d293df86ae72bd4b449efb47a6bb7ace
maki10 <nemanjammaric@gmail.com>
2016-09-29 13:27:46 +0200

    Fixed unique key issue in galleries migration

--- src/database/migrations/2016_09_28_141609_add_key_to_galleries_table.php
+++ src/database/migrations/2016_09_28_141609_add_key_to_galleries_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use SystemInc\LaravelAdmin\Gallery;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,15 @@ class AddKeyToGalleriesTable extends Migration
     public function up()
     {
         Schema::table('galleries', function (Blueprint $table) {
-            $table->string('key')->unique()->after('title');
+            $table->string('key')->after('title');
+        });
+
+        Gallery::orderBy('id')->each(function($gallery, $key){
+            Gallery::find($gallery->id)->update(['key' => 'gallery_'.$gallery->id]);
+        });
+
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->string('key')->unique()->change();
         });
     }
 
