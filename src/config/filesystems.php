00bc8f279cd033c5a741e85d416b645b6890424f
Nemanja Maric <nemanjammaric@gmail.com>
2017-10-16 10:48:13 +0000

    Apply fixes from StyleCI

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -3,12 +3,12 @@
 return [
     'disks' => [
 
-    	'uploads' => [
-	        'driver' => 'local',
-	        'root' => public_path('uploads'),
-	        'url' => env('APP_URL').'/uploads',
-	        'visibility' => 'public',
-	    ],
+        'uploads' => [
+            'driver'     => 'local',
+            'root'       => public_path('uploads'),
+            'url'        => env('APP_URL').'/uploads',
+            'visibility' => 'public',
+        ],
 
     ],
 
